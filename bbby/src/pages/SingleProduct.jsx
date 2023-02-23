bc93f29e85c08ae909ee85ebc135c7215b387e82
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-02-23 23:54:09 +0530

    day2

--- bbby/src/pages/SingleProduct.jsx
+++ bbby/src/pages/SingleProduct.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box, Flex, Grid, Image, Img } from '@chakra-ui/react'
+
 
 const SingleProduct = () => {
-  return (
-    <div>
-      singleproduct
-    </div>
+  const [product, setProduct] = useState({})
+
+  const { id } = useParams()
+  const fetchAndUpdateData = async (id) => {
+    try {
+      const data = await axios.get(`https://api.escuelajs.co/api/v1/products/${id}`)
+      // .then((res) => {
+      //   // console.log(res.data)
+      //   setProduct(res.data)
+      // })
+      console.log(data.data)
+      setProduct(data.data)
+    } catch (error) {
+     console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchAndUpdateData(id)
+  }, [id])
+
+  console.log(product)
+
+  return (<></>
+    // <>
+    //   <Flex>
+    //     <Grid templateColumns={"repeat(2,1fr)"} gap="20px">
+    //       {/* <Img src={product.images[0]}/> */}
+    //       <h1>{product.price}</h1>
+    //     </Grid>
+    //     <Box></Box>
+    //   </Flex>
+    // </>
   )
 }
 
